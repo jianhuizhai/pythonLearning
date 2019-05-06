fdbae9db4347c81a0884b5715c9c970ec7c21489
jianhuizhai <jianhuizhai89@gmail.com>
2019-05-06 15:14:51 +0200

    Create indexingSlicing.py

---
+++ beginner/string/indexingSlicing.py
@@ -0,0 +1,23 @@
+'''
+Enter the following into the interactive shell:
+'''
+
+spam = 'Hello world!'
+spam[0]
+
+spam[4]
+
+spam[-1]
+
+spam[0:5]   # 0 is included, 5 is not included
+
+spam[:5]
+
+spam[6:] # 6 is included
+
+fizz = spam[0:5]
+
+'Hello' in 'Hello World'
+'Hello' in 'Hello'
+'HELLO' in 'Hello World'
+'' in 'spam'
